eeb711c4cf255884bfe2661573b23c65e58bdb15
Daneo Van Overloop <daneo.vanoverloop@unleashed.be>
2017-06-14 10:17:45 +0200

    Product name is now uniform across targets. Allows for reusability between iOS and watchOS targets

--- libPhoneNumber-iOS/libPhoneNumber.h
+++ libPhoneNumber-iOS/libPhoneNumber.h
@@ -6,13 +6,16 @@
 //  Copyright (c) 2015 ohtalk.me. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
+#if TARGET_OS_WATCH
+    #import <WatchKit/WatchKit.h>
+#else
+    #import <UIKit/UIKit.h>
+#endif
 //! Project version number for libPhoneNumber-iOS.
-FOUNDATION_EXPORT double libPhoneNumber_iOSVersionNumber;
+FOUNDATION_EXPORT double libPhoneNumberVersionNumber;
 
 //! Project version string for libPhoneNumber-iOS.
-FOUNDATION_EXPORT const unsigned char libPhoneNumber_iOSVersionString[];
+FOUNDATION_EXPORT const unsigned char libPhoneNumberVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <libPhoneNumber_iOS/PublicHeader.h>
 
